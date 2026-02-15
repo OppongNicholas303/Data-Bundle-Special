ceee37a686e28bac258813d95749f47b9804872a
OppongNicholas303 <nicholas.oppong@amalitech.com>
2026-02-15 22:43:40 +0000

    add cors

--- space_bundle/src/main/java/com/space/space_bundle/out/config/SecurityConfig.java
+++ space_bundle/src/main/java/com/space/space_bundle/out/config/SecurityConfig.java
@@ -118,7 +118,7 @@ public class SecurityConfig {
 
         // In production, replace with actual frontend URLs
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
+                "https://tapdata.vercel.app/",
                 "http://localhost:4200",
                 "http://localhost:8081",
                 "https://jn4ktsmh-8081.uks1.devtunnels.ms",
