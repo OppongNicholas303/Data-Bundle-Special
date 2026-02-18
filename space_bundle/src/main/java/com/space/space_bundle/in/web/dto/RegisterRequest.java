96db0c496bcd9e3c29eda90b06973be7cd4c5f88
OppongNicholas303 <nicholas.oppong@amalitech.com>
2026-02-18 19:34:35 +0000

    usernamer fix

--- space_bundle/src/main/java/com/space/space_bundle/in/web/dto/RegisterRequest.java
+++ space_bundle/src/main/java/com/space/space_bundle/in/web/dto/RegisterRequest.java
@@ -20,7 +20,10 @@ public class RegisterRequest {
 
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
-    @Pattern(regexp = "^[a-zA-Z0-9_-]+$", message = "Username can only contain letters, numbers, underscores and hyphens")
+    @Pattern(
+            regexp = "^[a-zA-Z]+([ '-][a-zA-Z]+)*$",
+            message = "Username must be a valid full name"
+    )
     private String username;
 
     @NotBlank(message = "Email is required")
