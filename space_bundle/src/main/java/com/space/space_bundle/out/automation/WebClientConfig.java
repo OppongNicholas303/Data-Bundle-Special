ef7e829d9abefbf422a197a69e7267f02841da93
OppongNicholas303 <nicholas.oppong@amalitech.com>
2026-02-28 23:00:35 +0000

    webclient

---
+++ space_bundle/src/main/java/com/space/space_bundle/out/automation/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.space.space_bundle.out.automation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
