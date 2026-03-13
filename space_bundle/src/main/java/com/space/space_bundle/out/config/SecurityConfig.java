9ec35072bddcec56cd3e5f192410e8f768e16685
Nicholas Oppong <nicholas.oppong2623@gmail.com>
2026-03-13 14:29:54 +0000

    add tapdataa to the cors

--- space_bundle/src/main/java/com/space/space_bundle/out/config/SecurityConfig.java
+++ space_bundle/src/main/java/com/space/space_bundle/out/config/SecurityConfig.java
@@ -120,6 +120,7 @@ public class SecurityConfig {
         // In production, replace with actual frontend URLs
         configuration.setAllowedOrigins(List.of(
                 "https://tapdata.vercel.app/",
+                "https://tapdataa.vercel.app/",
                 "http://localhost:4200",
                 "http://localhost:8081",
                 "https://jn4ktsmh-8081.uks1.devtunnels.ms",
