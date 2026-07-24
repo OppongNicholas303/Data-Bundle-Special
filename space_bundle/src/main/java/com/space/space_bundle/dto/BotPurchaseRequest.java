0b6dbd814d7dc733783994b99711bc83390396a6
Nicholas Oppong <nicholas.oppong2623@gmail.com>
2026-07-24 15:05:25 +0000

    Revert BotPurchaseRequest JSON property

--- space_bundle/src/main/java/com/space/space_bundle/dto/BotPurchaseRequest.java
+++ space_bundle/src/main/java/com/space/space_bundle/dto/BotPurchaseRequest.java
@@ -1,6 +1,5 @@
 package com.space.space_bundle.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
@@ -10,7 +9,6 @@ import lombok.*;
 public class BotPurchaseRequest {
     private String network;
     private String beneficiary;
-    @JsonProperty("pa_data-bundle-packages")
     private Integer dataBundlePackages;
     private Integer package_id;
     private String customer_phone;
